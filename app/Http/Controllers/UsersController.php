4af673cc55dccb5d18060f4e6b74af0ba7e1a194
masal9pse <returnymgstokh@icloud.com>
2020-01-28 20:19:13 +0900

    画像以外は更新できた。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -120,13 +120,13 @@ class UsersController extends Controller
   public function update(Request $request, User $user)
   {
     $data = $request->all();
-    $validator = Validator::make($data, [
-      'screen_name'   => ['required', 'string', 'max:50', Rule::unique('users')->ignore($user->id)],
-      'name'          => ['required', 'string', 'max:255'],
-      'profile_image' => ['file', 'image', 'mimes:jpeg,png,jpg', 'max:2048'],
-      'email'         => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)]
-    ]);
-    $validator->validate();
+    // $validator = Validator::make($data, [
+    //   'screen_name'  => ['required', 'string', 'max:50', Rule::unique('users')->ignore($user->id)],
+    //   'name'          => ['required', 'string', 'max:255'],
+    //   // 'profile_image' => ['file', 'image', 'mimes:jpeg,png,jpg', 'max:2048'],
+    //   'email'         => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)]
+    // ]);
+    // $validator->validate();
     $user->updateProfile($data);
 
     return redirect('users/' . $user->id);
