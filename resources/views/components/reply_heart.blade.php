82865811eee87f64f9f0a3f51165100690e010f1
masal9pse <returnymgstokh@icloud.com>
2020-01-29 12:52:53 +0900

    click時に数字の切り替えを実装

--- resources/views/components/reply_heart.blade.php
+++ resources/views/components/reply_heart.blade.php
@@ -1,21 +1,27 @@
 {{-- 返信のいいね機能 --}}
-<div class="d-flex align-items-center">
-  @if (!in_array($user->id, array_column($tweet->favorites->toArray(), 'user_id'), TRUE))
-  <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
-    @csrf
+<div class="d-flex align-items-center" id="app">
+  <button type="submit" class="btn p-0 border-0 text-primary" @click="addHeart"><i
+      class="far fa-heart fa-fw"></i></button>
 
-    <input type="hidden" name="tweet_id" value="{{ $tweet->id }}">
-    <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
-  </form>
-  @else
-  <form method="POST"
-    action="{{ url('favorites/' .array_column($tweet->favorites->toArray(), 'id', 'user_id')[$user->id]) }}"
-    class="mb-0">
-    @csrf
-    @method('DELETE')
+  {{-- <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button> --}}
 
-    <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
-  </form>
-  @endif
-  <p class="mb-0 text-secondary">{{ count($tweet->favorites) }}</p>
+  <span>@{{ count }}</span>
 </div>
+
+<script>
+  var app = new Vue({
+el:'#app',
+data:{
+  count:0,
+},
+methods:{
+  addHeart(){
+    if(this.count === 0 ) {
+      this.count++;
+  }else{
+    this.count--;
+  }
+ }
+}
+});
+</script>
