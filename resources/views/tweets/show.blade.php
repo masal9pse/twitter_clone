b28bb72edbf4f9c03a2c6c686bb042f560d34c71
masal9pse <returnymgstokh@icloud.com>
2020-01-29 12:26:57 +0900

    返信のいいね機能のみコンポーネントで切り出した

--- resources/views/tweets/show.blade.php
+++ resources/views/tweets/show.blade.php
@@ -127,33 +127,11 @@
               {{-- コメント機能ここまで --}}
 
               <!-- 返信にいいね機能 -->
-              <div class="d-flex align-items-center">
-                @if (!in_array($user->id, array_column($tweet->favorites->toArray(), 'user_id'), TRUE))
-                <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
-                  @csrf
-
-                  <input type="hidden" name="tweet_id" value="{{ $tweet->id }}">
-                  <button type="submit" class="btn p-0 border-0 text-primary"><i
-                      class="far fa-heart fa-fw"></i></button>
-                </form>
-                @else
-                <form method="POST"
-                  action="{{ url('favorites/' .array_column($tweet->favorites->toArray(), 'id', 'user_id')[$user->id]) }}"
-                  class="mb-0">
-                  @csrf
-                  @method('DELETE')
-
-                  <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
-                </form>
-                @endif
-                <p class="mb-0 text-secondary">{{ count($tweet->favorites) }}</p>
-              </div>
-              <!-- いいねここまで -->
-              {{-- ここまでが自分が投稿したところ --}}
-
+              @include('components.reply_heart')
             </div>
-
           </div>
+          {{-- ここまでが自分が投稿したところ --}}
+
         </li>
         @empty
         <li class="list-group-item">
