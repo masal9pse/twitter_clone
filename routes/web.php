2bf392e5e156e6ba23c139822023ef99840b7f27
masal9pse <returnymgstokh@icloud.com>
2020-02-04 17:07:51 +0900

    logoutエラー419を解決

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@
 
 Auth::routes();
 
-Route::get('/', 'PostController@index')->name('posts.index');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
   Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'edit', 'update']]);
