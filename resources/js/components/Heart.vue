8aeb894e9ec0608ef1ea3a1a49f5d5ee58937294
masal9pse <returnymgstokh@icloud.com>
2020-02-06 17:57:01 +0900

    フロントだけなら色も変えてできた。

---
+++ resources/js/components/Heart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!-- <div class="d-flex align-items-center"> -->
+    <button v-if="!Heart" type="submit" class="btn p-0 border-0 text-primary" @click="addLike">
+      <i class="far fa-heart fa-fw"></i>
+      {{ count }}
+    </button>
+    <button v-else type="submit" class="btn p-0 border-0 text-danger">
+      <i class="fas fa-heart fa-fw" @click="addLike"></i>
+      {{ count }}
+    </button>
+    <span></span>
+    <!-- </div> -->
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      Heart: true,
+      count: 0
+    };
+  },
+  methods: {
+    addLike() {
+      // document.addEventListener("DOMContentLoaded", function(event) {
+      // 元々記述のあった処理をaddEventListenerのカッコで挟む
+      if (this.count === 0) {
+        this.count++;
+        this.Heart = false;
+      } else {
+        this.count--;
+        this.Heart = true;
+      }
+      // });
+    }
+  }
+};
+</script>
