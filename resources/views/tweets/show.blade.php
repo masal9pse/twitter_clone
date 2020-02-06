3e7fba731faefc4288294c0c0a2ac06ba49babd0
masal9pse <returnymgstokh@icloud.com>
2020-02-06 18:54:19 +0900

    マイグレート、モデル作成

--- resources/views/tweets/show.blade.php
+++ resources/views/tweets/show.blade.php
@@ -136,9 +136,6 @@
               <!-- 返信にいいね機能 -->
               {{-- @include('components.reply_like') --}}
               <span>
-                {{-- <like :post-id="{{ json_encode($post->id) }}" :user-id="{{ json_encode($userAuth->id) }}"
-                :default-Liked="{{ json_encode($defaultLiked) }}" :default-Count="{{ json_encode($defaultCount) }}">
-                </like> --}}
                 <heart></heart>
               </span>
             </div>
