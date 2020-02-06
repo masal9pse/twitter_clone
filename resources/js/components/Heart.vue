a0f59f523a0d7ad7b8a80f75aadcd43dd4f68f60
masal9pse <returnymgstokh@icloud.com>
2020-02-06 18:01:48 +0900

    フロントのみ正常起動

--- resources/js/components/Heart.vue
+++ resources/js/components/Heart.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div class="d-flex align-items-center"> -->
     <button v-if="!Heart" type="submit" class="btn p-0 border-0 text-primary" @click="addLike">
       <i class="far fa-heart fa-fw"></i>
       {{ count }}
@@ -9,30 +8,25 @@
       <i class="fas fa-heart fa-fw" @click="addLike"></i>
       {{ count }}
     </button>
-    <span></span>
-    <!-- </div> -->
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      Heart: true,
+      Heart: false,
       count: 0
     };
   },
   methods: {
     addLike() {
-      // document.addEventListener("DOMContentLoaded", function(event) {
-      // 元々記述のあった処理をaddEventListenerのカッコで挟む
       if (this.count === 0) {
         this.count++;
-        this.Heart = false;
+        this.Heart = true;
       } else {
         this.count--;
-        this.Heart = true;
+        this.Heart = false;
       }
-      // });
     }
   }
 };
