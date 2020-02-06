9a4851e7176830dba9392cbc9081d1261ef8670d
masal9pse <returnymgstokh@icloud.com>
2020-02-06 20:00:18 +0900

    紐づけるファイルはcommentsTableではなくtweetsTable

--- app/Http/Controllers/HeartController.php
+++ app/Http/Controllers/HeartController.php
@@ -10,20 +10,20 @@ class HeartController extends Controller
 {
   public function heart(Comment $comment, Request $request)
   {
-    $heart = Like::create(['user_id' => $request->user_id, 'tweet_id' => $comment->id]);
+    $heart = Heart::create(['user_id' => $request->user_id, 'tweet_id' => $comment->id]);
     // dd($heart);
 
-    $heartCount = count(Like::where('tweet_id', $comment->id)->get());
+    $heartCount = count(Heart::where('tweet_id', $comment->id)->get());
 
     return response()->json(['heartCount' => $heartCount]);
   }
 
   public function unheart(Comment $comment, Request $request)
   {
-    $heart = Like::where('user_id', $request->user_id)->where('tweet_id', $comment->id)->first();
+    $heart = Heart::where('user_id', $request->user_id)->where('tweet_id', $comment->id)->first();
     $heart->delete();
 
-    $heartCount = count(Like::where('tweet_id', $comment->id)->get());
+    $heartCount = count(Heart::where('tweet_id', $comment->id)->get());
 
     return response()->json(['likeCount' => $heartCount]);
   }
--- resources/views/tweets/show.blade.php
+++ resources/views/tweets/show.blade.php
@@ -9,7 +9,6 @@
         <a href="{{ url('users') }}">ユーザ一覧 <i class="fas fa-users" class="fa-fw"></i> </a>
         <a href="{{ route('posts.index') }}" class="ml-3">post一覧へ<i class="fas fa-users" class="fa-fw"></i></a>
       </div>
-
       <div class="card">
         <div class="card-haeder p-3 w-100 d-flex">
           <img src="{{ $tweet->user->profile_image }}" class="rounded-circle" width="50" height="50">
